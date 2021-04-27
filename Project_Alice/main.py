9c97006a8ff02f5cd32043884c0dab13ed5718fa
RinatKhamitov <74911501+RinatKhamitov@users.noreply.github.com>
2021-04-27 15:15:09 +0300

    Update main.py

--- Project_Alice/main.py
+++ Project_Alice/main.py
@@ -2,10 +2,8 @@ import json
 import random
 
 from flask import Flask, request
-from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
-run_with_ngrok(app)
 
 player_class = {
     'rogue': {
